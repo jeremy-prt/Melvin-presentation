0ac8c387d8d7bb20c3cb7b7c0b65471e37c6eab4
Jeremy <jeremyperret1@gmail.com>
2024-02-17 15:13:51 +0100

    Hero page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#cd005c",
+        secondary: "#ff92c9",
+      },
+      fontFamily: {
+        main: ["GeneralSans-Variable"],
+      },
+      fontSize: {
+        title: ["clamp(2rem, 8vw, 9rem)"],
+        subtitle: ["clamp(1rem, 7vw, 7rem)"],
+      },
+    },
   },
   plugins: [],
 };
